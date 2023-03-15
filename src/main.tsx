6d0b2eec904ac81f9fccc29916a512f2b3ee2443
Eugen Svirbutovich <4workacc@gmail.com>
2023-03-15 12:11:24 +0300

    feat: simple routing

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App/App'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import AboutUs from './Pages/AboutUs';
+import NotFound404 from './Pages/NotFound404';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound404 />
+  },
+  {
+    path: "/about",
+    element: <AboutUs />,
+    errorElement: <NotFound404 />
+  },
+
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
