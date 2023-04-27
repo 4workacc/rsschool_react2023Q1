f43fb6010c2add463de2abe446565f5912b71110
Eugen Svirbutovich <4workacc@gmail.com>
2023-04-27 16:14:38 +0300

    feat: some tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,11 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 import coverage from '@cypress/code-coverage/task';
 
 export default defineConfig({
-  video: false,  
-  e2e: {    
-    experimentalRunAllSpecs: true,    
+  video: false,
+  e2e: {
+    baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
       coverage(on, config);
       return config;
     },
--- vite.config.ts
+++ vite.config.ts
@@ -7,19 +7,22 @@
 // })
 
 
-
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import istanbul from 'vite-plugin-istanbul';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), istanbul({
+    cypress: true,
+    requireEnv: false,
+  })],
   test: {
     globals: true,
     environment: 'jsdom', 
     coverage: {
       reporter: ['text'],
-      all: true,
+      all: true,      
    }
   }
 })
