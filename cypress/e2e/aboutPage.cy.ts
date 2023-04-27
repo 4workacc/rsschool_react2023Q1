925f7490b5dd37a2fba8df7a74e4297b56b16c18
Eugen Svirbutovich <4workacc@gmail.com>
2023-04-27 17:54:57 +0300

    fix: instrument??

--- cypress/e2e/aboutPage.cy.ts
+++ cypress/e2e/aboutPage.cy.ts
@@ -1,5 +1,5 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000/about')
+  it('page visited', () => {
+    cy.visit('http://localhost:3000/aboutUs');
   });  
 })
